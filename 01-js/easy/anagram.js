6a842ccb4c899b01ea1fa30476b429992b90c4b3
Nitin Kumar Pal <nitinpal900@gmail.com>
2024-01-10 13:58:33 +0530

    anagran

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,28 @@
 */
 
 function isAnagram(str1, str2) {
+  let frq = Array(128).fill(0);
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
 
+  for(let i=0; i< str1.length; i++){
+    frq[str1.charCodeAt(i)-'a'.charCodeAt(0)]++;
+  }
+
+  for(let i=0; i< str2.length; i++){
+    frq[str2.charCodeAt(i)-'a'.charCodeAt(0)]--;
+  }
+  if(str1.length!=str2.length){
+    return false;
+  }
+
+  for(let i=0; i<128; i++){
+    if(frq[i]!=0){
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
