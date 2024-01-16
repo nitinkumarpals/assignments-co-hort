6db5070c2cc606d6f656e3d938860a0a1f51d0db
Nitin Kumar Pal <nitinpal900@gmail.com>
2024-01-16 18:57:24 +0530

    hard-assignments-promise-week-2

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,31 @@
  */
 
 function wait1(t) {
-
+    return new Promise(resolve => setTimeout(resolve, t*1000));
 }
 
 function wait2(t) {
-
+    return new Promise(resolve => setTimeout(resolve, t*1000));
 }
 
 function wait3(t) {
-
+    return new Promise(resolve => setTimeout(resolve, t*1000));
 }
 
-function calculateTime(t1, t2, t3) {
+async function calculateTime(t1, t2, t3) {
+    const startTime = Date.now();
 
+    const promise1 = wait1(t1);
+    const promise2 = wait1(t2);
+    const promise3 = wait1(t3);
+
+    await wait1(t1);
+    await wait2(t2);
+    await wait3(t3);
+
+    const endTime = Date.now();
+    const totalTime = endTime - startTime;
+    return totalTime;
 }
 
 module.exports = calculateTime;
