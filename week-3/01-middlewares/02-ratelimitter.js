60c4c697f1a98e08551dfa833546e02426b848b2
Nitin Kumar Pal <nitinpal900@gmail.com>
2024-01-25 19:34:53 +0530

    completed-all-middleware-assignments

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,22 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function(req,res,next){
+  const userId = req.headers["user-id"];
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId]++;
+    if(numberOfRequestsForUser[userId]>5){
+      res.status(404).send("No entry limit exceeded");
+    }
+    else{
+      next();
+    }
+  }
+  else{
+    numberOfRequestsForUser[userId] =1;
+    next();
+  }
+});
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
